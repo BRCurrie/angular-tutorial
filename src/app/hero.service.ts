d3dbdca2ed4082534ae65e02adfa5e09f17ec499
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-01-07 08:58:36 -0600

    Part 6 Complete

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,6 +5,10 @@ import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap} from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -45,4 +49,39 @@ export class HeroService {
     private log(message: string) {
       this.messageService.add(`HeroService: $(message)`);
     }
+
+    updateHero (hero: Hero): Observable<any> {
+      return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+    }
+
+    addHero (hero: Hero): Observable<Hero> {
+      return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+        tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+    }
+
+    deleteHero (hero: Hero | number): Observable<Hero> {
+      const id = typeof hero === 'number' ? hero : hero.id;
+      const url = `${this.heroesUrl}/${id}`;
+
+      return this.http.delete<Hero>(url, httpOptions).pipe(
+        tap(_ => this.log(`deleted hero id=#{id}`)),
+        catchError(this.handleError<Hero>('deleteHero'))
+      );
+    }
+
+    searchHeroes(term: string): Observable<Hero[]> {
+      if (!term.trim()) {
+        return of ([]);
+      }
+      return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+        tap(_ => this.log(`found heroes matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      );
+    }
+
 }
